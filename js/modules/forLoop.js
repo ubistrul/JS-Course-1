24f957d2ae29170cee12a7c6921491907f9afeb7
Alexey Serduchenko <ubistrul@gmail.com>
2023-10-26 01:35:21 +0300

    Clicker upd
    Fixed adaptive layout

--- js/modules/forLoop.js
+++ js/modules/forLoop.js
@@ -1,8 +1,8 @@
 function repeat(text, count) {
-    for (let i = 1; i < count; i++) {
-    text += text;
-    }
-    return text
+	for (let i = 1; i < count; i++) {
+		text += text;
+	}
+	return text;
 }
 
 console.log(repeat('wow', 3));
@@ -10,5 +10,5 @@ console.log(repeat('wow', 3));
 const numbers = [1, 3, 8, 9, 100, 23, 55, 34];
 
 // BEGIN (write your solution here)
-const getEvenNumbers = numbers.filter((number) => number % 2 === 0)
+const getEvenNumbers = numbers.filter((number) => number % 2 === 0);
 console.log(getEvenNumbers);
---
+++ js/modules/primitiveCalc.js
@@ -0,0 +1,11 @@
+const name = 'Alexey';
+
+console.log(`Hello my name is ${name}`);
+
+
+const cost = ((1 * 500) + (2 * 300) + (1 * 900)) / 100;
+const fee = (cost * 0.2); 
+const costWithFee = cost - fee;
+console.log(`Стоимость еды: $${cost}`);
+console.log(`Стоимость еды, учитывая комиссию: $${costWithFee}`);
+
